b8c5952d9127c1c98037574b627b041a5c1bdf56
KaydenF-529781 <kbfuchs06@gmail.com>
2025-01-22 10:31:30 -0600

    Lab1 - Submitted

--- edit_me.py
+++ edit_me.py
@@ -4,9 +4,9 @@ def find_max(lst):
 
     current_max = lst[0]
 
-    #
-    #  YOUR CODE GOES HERE
-    #
+    for i in lst:
+        if lst[i] > current_max:
+            current_max = lst[i]
 
     return current_max
 
